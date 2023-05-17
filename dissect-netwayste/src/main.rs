ac985ebede08736532c63ed01b0c62b93440f34c
mang <manghirs@gmail.com>
2023-05-16 22:25:56 -0700

    print source ip and port in output

--- dissect-netwayste/src/main.rs
+++ dissect-netwayste/src/main.rs
@@ -1,7 +1,7 @@
 use bincode::deserialize;
 use clap::{self, Parser};
-use etherparse::{SlicedPacket, TransportSlice::Udp};
-use netwaystev2::{protocol::Packet, DEFAULT_PORT};
+use etherparse::{InternetSlice::Ipv4, SlicedPacket, TransportSlice::Udp};
+use netwaystev2::{protocol::Packet, DEFAULT_PORT as NETWAYSTE_PORT};
 use pcap;
 use tracing::*;
 use tracing_subscriber::FmtSubscriber;
@@ -15,7 +15,7 @@ struct Args {
 
 fn main() {
     let subscriber = FmtSubscriber::builder()
-        // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.) will be written to stdout.
+        // All spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.) will be written to stdout.
         .with_max_level(Level::TRACE)
         .finish();
 
@@ -26,7 +26,7 @@ fn main() {
     // Verify we can find a device
     let _ = pcap::Device::list().expect("device lookup failed");
 
-    // get the default Device
+    // Get the default Device
     let device = pcap::Device::lookup()
         .expect("device lookup failed")
         .expect("no device available");
@@ -40,23 +40,37 @@ fn main() {
         .open()
         .unwrap();
 
-    cap.filter(format!("udp port {:?}", DEFAULT_PORT).as_str(), true)
+    cap.filter(format!("udp port {:?}", NETWAYSTE_PORT).as_str(), true)
         .expect("failed to filter for netwayste packets");
 
     while let Ok(packet) = cap.next_packet() {
         match SlicedPacket::from_ethernet(packet.data) {
             Err(err) => {
-                panic!("deserializing EthernetII packet: {}", err);
+                if args.verbose {
+                    error!("Failed EthernetII packet de-serialization: '{}'", err);
+                }
             }
             Ok(ethernet) => {
+                let src_port;
+                let src_ip;
+
                 match ethernet.transport {
+                    // Filter away non-netwayste packets based on the source and destination port
                     Some(Udp(udp)) => {
-                        // Filter away non-netwayste packets based on the source and destination port
-                        if udp.source_port() != DEFAULT_PORT
-                            && udp.destination_port() != DEFAULT_PORT
+                        if udp.source_port() != NETWAYSTE_PORT
+                            && udp.destination_port() != NETWAYSTE_PORT
                         {
                             continue;
                         }
+
+                        src_port = udp.source_port();
+                    }
+                    _ => continue,
+                }
+
+                match ethernet.ip {
+                    Some(Ipv4(ipv4, _extensions)) => {
+                        src_ip = ipv4.source_addr();
                     }
                     _ => continue,
                 }
@@ -64,7 +78,7 @@ fn main() {
                 // There's a packet that is candidate for matching netwayste
                 match deserialize::<Packet>(ethernet.payload) {
                     Ok(nw_packet) => {
-                        info!("{:?}", nw_packet);
+                        info!("{:>15?}:{:<5} {:?}", src_ip, src_port, nw_packet);
                     }
                     Err(e) => {
                         if args.verbose {
